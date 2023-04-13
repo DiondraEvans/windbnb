a9bc00600938462d2164a219d114a9f5a0d10505
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-04-12 20:52:18 -0400

    fixed the session-info endpoint

--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -12,7 +12,7 @@ export const logIn = async (formData) => {
 return serverResponse;
 } 
 export const getUserFromSession = async () => {
-    let response = await axios('https://wind-bnb-website-api.vercel.app//session-info')
+    let response = await axios('https://wind-bnb-website-api.vercel.app/session-info')
     console.log(response);
     console.log(`hello, this is your session secret, so we can rule this out ${process.env.SESSION_SECRET}`)
     // WE HAVE THE LOGGED IN USER! :)
