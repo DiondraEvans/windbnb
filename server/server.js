f29cab9590c79cbad78ce531d7e9aa16540049ce
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-04-12 23:01:03 -0400

    testing server

--- server/server.js
+++ server/server.js
@@ -82,10 +82,10 @@ app.use(session({
 }))
 
 app.get('/session-info', (req, res) => {
-    res.json({
-        session: req.session
-    });
-    
+    // res.json({
+    //     session: req.session
+    // });
+    console.log("hi this is your server")
 });
 
 
