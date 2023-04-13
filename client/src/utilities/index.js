cce3ee94547e1ced196ee07b9fdb6660a4f9934e
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-04-13 01:27:29 -0400

    test

--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -4,7 +4,7 @@ export const logIn = async (formData) => {
     console.log(formData)
     let serverResponse = await axios({
         method: "PUT",
-        url: "https://wind-bnb-website-api.vercel.app/users/login",
+        url: "/users/login",
         data: formData
     });
     console.log(serverResponse);
@@ -13,7 +13,7 @@ return serverResponse;
 } 
 
 export const getUserFromSession = async () => {
-    let response = await axios('https://wind-bnb-website-api.vercel.app/session-info')
+    let response = await axios('/session-info')
     console.log(response);
     // WE HAVE THE LOGGED IN USER! :)
     // if (response.data.session.passport) {
