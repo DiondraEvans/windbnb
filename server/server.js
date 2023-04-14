2f5c01d75e1093e2de389867cdaf7f111e8ad729
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-04-13 23:08:28 -0400

    port defined vercel

--- server/server.js
+++ server/server.js
@@ -209,6 +209,6 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000, () => {
-    console.log(`Server is Listening on 5000`)
-});
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is listening on port ${process.env.PORT || 5000}`);
+  });
