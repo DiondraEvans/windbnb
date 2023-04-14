6567441c66bd3b8a62c00d2d0379b24a0b75a951
Diondra <98505341+Diondraj@users.noreply.github.com>
2023-04-14 02:33:07 -0400

    res.json turns to res.send

--- server/server.js
+++ server/server.js
@@ -84,7 +84,7 @@ app.get('/session-info', (req, res) => {
         res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.json({ session: req.session });
+      res.send(`hi ${req.session}`);
     } else {
       res.status(401).json({ message: 'User not authenticated' });
     }
